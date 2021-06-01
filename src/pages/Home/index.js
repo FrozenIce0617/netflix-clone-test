2c6a09b840a61ddb4bcb909f05d5051039c3ac16
SuccessTracker <prosdev087@gmail.com>
2021-06-01 20:41:03 +0800

    remove un-used vars

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
 import { useApp } from "../../context";
 import ThumbsCard from "../../components/ThumbsCard";
